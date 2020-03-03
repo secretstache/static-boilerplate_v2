e1141485863e201f878bc155b54db03a8cfaed09
Victoria Naumova <nauorama@gmail.com>
2020-03-03 11:02:03 +0200

    updates

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -7,55 +7,61 @@ import '../../../node_modules/slick-carousel/slick/slick.js';
 
 $(document).foundation();
 
-$('[data-slick]').slick();
+$(document).ready(function() {
+    $('[data-slick]').slick();
 
-/* Sticky header */
-window.onscroll = function() { stickyHeader() };
-const header = document.querySelector('.site-header');
-var sticky = header.offsetTop;
+    /* Sticky header */
+    window.onscroll = function() { stickyHeader() };
+    const header = document.querySelector('.site-header');
+    var sticky = header.offsetTop;
 
-function stickyHeader() {
-    if (window.pageYOffset > sticky) {
-        header.classList.add("sticky");
-    } else {
-        header.classList.remove("sticky");
+    function stickyHeader() {
+        if (window.pageYOffset > sticky) {
+            header.classList.add("sticky");
+        } else {
+            header.classList.remove("sticky");
+        }
     }
+
+    /* Replace all SVG images with inline SVG */
+    function editableSvg(el) {
+        $(el).each(function() {
+            var $img = $(this);
+            var imgID = $img.attr('id');
+            var imgClass = $img.attr('class');
+            var imgURL = $img.attr('src');
+            $.get(imgURL, function(data) {
+                // Get the SVG tag, ignore the rest
+                var $svg = $(data).find('svg');
+                // Add replaced image's ID to the new SVG
+                if (typeof imgID !== 'undefined') {
+                    $svg = $svg.attr('id', imgID);
+                }
+                // Add replaced image's classes to the new SVG
+                if (typeof imgClass !== 'undefined') {
+                    $svg = $svg.attr('class', imgClass + ' replaced-svg');
+                }
+                // Remove any invalid XML tags as per http://validator.w3.org
+                $svg = $svg.removeAttr('xmlns:a');
+                // Check if the viewport is set, if the viewport is not set the SVG wont't scale.
+                if (!$svg.attr('viewBox') && $svg.attr('height') && $svg.attr('width')) {
+                    $svg.attr('viewBox', '0 0 ' + $svg.attr('width') + ' ' + $svg.attr('height'));
+                }
+                // Replace image with new SVG
+                $img.replaceWith($svg);
+            }, 'xml');
+        });
+    }
+
+    editableSvg('img.editable-svg');
+
+    /* Hamburger click handle */
+    $('.hamburger').on('click', function() {
+        $('body').toggleClass('offcanvas-active');
+    });
+
+    /* Offcanvas close button handle */
+    $('.off-canvas .close-button').on('click', function() {
+        $('body').removeClass('offcanvas-active');
+    });
 }
-
-/*
- * Replace all SVG images with inline SVG
- */
-$('img.editable-svg').each(function() {
-    var $img = $(this);
-    var imgID = $img.attr('id');
-    var imgClass = $img.attr('class');
-    var imgURL = $img.attr('src');
-    $.get(imgURL, function(data) {
-        // Get the SVG tag, ignore the rest
-        var $svg = $(data).find('svg');
-        // Add replaced image's ID to the new SVG
-        if (typeof imgID !== 'undefined') {
-            $svg = $svg.attr('id', imgID);
-        }
-        // Add replaced image's classes to the new SVG
-        if (typeof imgClass !== 'undefined') {
-            $svg = $svg.attr('class', imgClass + ' replaced-svg');
-        }
-        // Remove any invalid XML tags as per http://validator.w3.org
-        $svg = $svg.removeAttr('xmlns:a');
-        // Check if the viewport is set, if the viewport is not set the SVG wont't scale.
-        if (!$svg.attr('viewBox') && $svg.attr('height') && $svg.attr('width')) {
-            $svg.attr('viewBox', '0 0 ' + $svg.attr('height') + ' ' + $svg.attr('width'));
-        }
-        // Replace image with new SVG
-        $img.replaceWith($svg);
-    }, 'xml');
-});
-
-$('.hamburger').on('click', function() {
-    $('body').toggleClass('offcanvas-active');
-})
-
-$('.off-canvas .close-button').on('click', function() {
-    $('body').removeClass('offcanvas-active');
-})
